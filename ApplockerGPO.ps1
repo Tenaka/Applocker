ed32715030dcd03df1a86c59763ee88f711cfa88
Tenaka <86342641+Tenaka@users.noreply.github.com>
2022-06-28 20:18:09 +0100

    Update ApplockerGPO.ps1

--- ApplockerGPO.ps1
+++ ApplockerGPO.ps1
@@ -1,17 +1,20 @@
-﻿
+
 #Working Folder on local client
 
-$GPOName = Read-Host "Name of the Applocker GPO....."
+#$GPOName = Read-Host "Name of the Applocker GPO....."
 
+$GPOName = "DenyTheWorld"
 $path = "c:\downloads\Applocker"
+
 New-Item -Path $path -ItemType Directory -Force
 
 #Output files
 $xmlExe = "$path\Exe.xml"
 $xmlScript = "$path\Script.xml"
 $xmlMSI = "$path\Msi.xml"
-$xmlPack = "$path\Appx.xml"
+$xmlPack = "$path\PackagedRules.xml"
 $xmlPath = "$path\PathRules.xml"
+$xmldnyWin = "$path\dnyWin.xml"
 $xmldnyWin32 = "$path\dnyWin32.xml"
 $xmldnyWin64 = "$path\dnyWin64.xml"
 $xmldnyProg32 = "$path\dnyProg32.xml"
@@ -19,22 +22,24 @@ $xmldnyProg64 = "$path\dnyProg64.xml"
 $xmldnyWinSxs = "$path\dnyWinSxs.xml"
 $xmldnyPS = "$path\dnyPS.xml"
 
-#The following list of files are commonly abused to bypass Applocker or attack the system.
-$dny = "gathernetworkInf.vbs", 
-"installutil.Exe", #Applocker Bypass
+#Block the following files
+$dny = 
+"gathernetworkInf.vbs", #system info gather
+"installutil.Exe", #Applocker bypass
 "scrobj.dll",
 "sos.dll",
-"cipher.Exe", 
-"certutil.exe",  #Used to download files and encode text to programs 
-<#Microsoft recommended files to block
-https://docs.microsoft.com/en-us/windows/security/threat-protection/windows-defender-application-control/microsoft-recommended-block-rules
-#>
+"cipher.Exe", #Used by malware to permanently delete or encrypt files 
+"certutil.exe",  #Used by hackers to download files and encode text, base64
+"csc.exe",    #Used in conjunction with InstallUtil.exe to csharp to exe a file
+"cmd.exe",  #Creates data streams
+"ExtExport.exe",  #Loads and executes dll's from other folders and used as part of Astaroth malware
+#MS Recommended
 "addinprocess.exe",
 "addinprocess32.exe",
 "addinutil.exe",
 "aspnet_compiler.exe",
 "bash.exe",
-"bginfo.exe"
+"bginfo.exe",
 "cdb.exe",
 "csi.exe",
 "dbghost.exe",
@@ -63,7 +68,8 @@ https://docs.microsoft.com/en-us/windows/security/threat-protection/windows-defe
 "visualuiaverifynative.exe",
 "wfc.exe",
 "windbg.exe",
-"wmic.exe"
+"wmic.exe",
+"msdt.exe" 
 
 $dnyPSExe =
 'Powershell.exe',
@@ -73,7 +79,7 @@ $dnyPSExe =
 #Create policy for all Exe's
 Get-ChildItem C:\ -Force -Recurse -ErrorAction SilentlyContinue | 
 where {$_.Extension -eq ".exe"} | 
-ForEach-Object {$_.FullName} | 
+ForEach-Object {$_.FullName   } | 
 Get-AppLockerFileInformation -ErrorAction SilentlyContinue | 
 New-AppLockerPolicy -RuleType Publisher,Hash -Optimize -Xml |
 Out-File $xmlExe
@@ -92,25 +98,34 @@ Out-File $xmlScript
 
 #Create policy for all Scripts
 Get-ChildItem C:\ -Force -Recurse -ErrorAction SilentlyContinue | 
-where {$_.Extension -eq ".msi" `
--or $_.Extension -eq ".msp*"} | 
+where {$_.Extension -eq ".msi" -or $_.Extension -eq ".msp*"} | 
 ForEach-Object {$_.FullName} | 
 Get-AppLockerFileInformation -ErrorAction SilentlyContinue | 
 New-AppLockerPolicy -RuleType Publisher,Hash -Optimize -Xml |
 Out-File $xmlMsi
 
-#Create policy for All Users and Packages (Appx)
-Get-AppxPackage -allusers | 
-Get-AppLockerFileInformation | 
-New-AppLockerPolicy -RuleType publisher -Optimize -xml -User Users |
-Out-File $xmlAppX 
+$ardnyWin=@()
+foreach ($dnyWin in $dny)
+    {
+    $dnyWinFileInf = Get-ChildItem -Path C:\Windows\ -Recurse -Force -ErrorAction SilentlyContinue | 
+    Where {$_.DirectoryName -notlike "*sysWOW64*" -and $_.DirectoryName -notlike "*system32*" -and $_.DirectoryName -notlike "*winsxs*" -and $_.name -eq "$dnyWin"} 
+    $dnyWinFullname = $dnyWinFileInf.FullName
+    $dnyWinFullname 
+    $ardnyWin += $dnyWinFullname
+    }
+sleep 5
+ForEach-Object {$ardnyWin} |
+Get-AppLockerFileInformation -ErrorAction SilentlyContinue | 
+New-AppLockerPolicy -RuleType Hash -Xml | 
+Out-File $xmldnyWin
 
 $ardnyWin32=@()
 foreach ($dnyWin32 in $dny)
     {
-    $dnyWin32FileInf = Get-ChildItem -Path C:\Windows\system32\ -Recurse -Force -ErrorAction SilentlyContinue | 
+    $dnyWin32FileInf = Get-ChildItem -Path C:\Windows\system32 -Recurse -Force -ErrorAction SilentlyContinue | 
     Where {$_.name -eq "$dnyWin32"} 
     $dnyWin32Fullname = $dnyWin32FileInf.FullName
+    $dnyWin32Fullname 
     $ardnyWin32 += $dnyWin32Fullname
     }
 sleep 5
@@ -122,7 +137,7 @@ Out-File $xmldnyWin32
 $ardnyWin64=@()
 foreach ($dnyWin64 in $dny)
     {
-    $dnyWin64FileInf = Get-ChildItem -Path C:\Windows\sysWOW64\ -Recurse -Force -ErrorAction SilentlyContinue | 
+    $dnyWin64FileInf = Get-ChildItem -Path C:\Windows\sysWOW64 -Recurse -Force -ErrorAction SilentlyContinue | 
     Where {$_.name -eq "$dnyWin64"}
     $dnyWin64Fullname = $dnyWin64FileInf.FullName
     $ardnyWin64 += $dnyWin64Fullname
@@ -161,7 +176,7 @@ Out-File $xmldnyProg32
 $ardnyWinSxs=@()
 foreach ($dnyWinSxs in $dny)
     {
-    $dnyWinSxsFileInf = Get-ChildItem -Path C:\Windows\winSxs\ -Recurse -Force -ErrorAction SilentlyContinue | 
+    $dnyWinSxsFileInf = Get-ChildItem -Path C:\Windows\winSxs -Recurse -Force -ErrorAction SilentlyContinue | 
     Where {$_.name -eq "$dnyWinSxs"}
     $dnyWinSxsFullname = $dnyWinSxsFileInf.FullName
     $ardnyWinSxs += $dnyWinSxsFullname
@@ -198,13 +213,9 @@ $gcXmlScript = get-content $xmlScript
 $gcXmlScript.Replace("NotConfigured","Enabled") | 
 Out-File $xmlScript
 
-$gcPack = get-content $xmlPack
-$gcPack.Replace("NotConfigured","Enabled") | 
-Out-File $xmlPack
-
-$gcPath = get-content $xmlPath
-$gcPath.Replace("NotConfigured","Enabled") | 
-Out-File $xmlPath
+$gcdnyWin = get-content $xmldnyWin
+$gcdnyWin.Replace("Allow","Deny").Replace("NotConfigured","Enabled") | 
+Out-File $xmldnyWin
 
 $gcdnyWin32 = get-content $xmldnyWin32
 $gcdnyWin32.Replace("Allow","Deny").Replace("NotConfigured","Enabled") | 
@@ -226,6 +237,14 @@ $gcdnyProg32 = get-content $xmldnyProg32
 $gcdnyProg32.Replace("Allow","Deny").Replace("NotConfigured","Enabled") | 
 Out-File $xmldnyProg32
 
+$ccPack = get-content $xmlPack
+$ccPack.Replace("NotConfigured","Enabled") | 
+Out-File $xmlPack
+
+$ccPath = get-content $xmlPath
+$ccPath.Replace("NotConfigured","Enabled") | 
+Out-File $xmlPath
+
 $gcdnyPS = get-content $xmldnyPS
 $gcdnyPS.Replace("Allow","Deny").Replace("NotConfigured","Enabled") | 
 Out-File $xmldnyPS
@@ -245,16 +264,35 @@ $cnPath = (Get-GPO -Name $GPOPath).path
 $dom=(Get-ADDomainController).hostname
 
 Set-AppLockerPolicy -XmlPolicy $xmlExe -Merge -Ldap "LDAP://$dom/$cn"
+sleep 5
 Set-AppLockerPolicy -XmlPolicy $xmlScript -Merge -Ldap "LDAP://$dom/$cn"
+sleep 5
 Set-AppLockerPolicy -XmlPolicy $xmlMSI -Merge -Ldap "LDAP://$dom/$cn"
+sleep 5
 Set-AppLockerPolicy -XmlPolicy $xmlPath -Merge -Ldap "LDAP://$dom/$cnPath"
+sleep 5
 Set-AppLockerPolicy -XmlPolicy $xmlPack -Merge -Ldap "LDAP://$dom/$cn"
+sleep 5
 
+Set-AppLockerPolicy -XmlPolicy $xmldnyWin -Merge -Ldap "LDAP://$dom/$cn"
+sleep 5
 Set-AppLockerPolicy -XmlPolicy $xmldnyWin32 -Merge -Ldap "LDAP://$dom/$cn"
+sleep 5
 Set-AppLockerPolicy -XmlPolicy $xmldnyWin64 -Merge -Ldap "LDAP://$dom/$cn"
+sleep 5
 Set-AppLockerPolicy -XmlPolicy $xmldnyWinSxs -Merge -Ldap "LDAP://$dom/$cn"
+sleep 5
 Set-AppLockerPolicy -XmlPolicy $xmldnyProg64 -Merge -Ldap "LDAP://$dom/$cn"
+sleep 5
 Set-AppLockerPolicy -XmlPolicy $xmldnyProg32 -Merge -Ldap "LDAP://$dom/$cn"
+sleep 5
 Set-AppLockerPolicy -XmlPolicy $xmldnyPS -merge -Ldap "LDAP://$dom/$cnAdmin"
+sleep 5
+
+
+
+
+
+
 
 
